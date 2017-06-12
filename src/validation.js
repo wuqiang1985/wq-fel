a9a8775f01068ee1db54e199fe877676fb281f98
wuqiang1985 <112055730@qq.com>
2017-06-12 15:58:10 +0800

    扩展validation模块

--- src/validation.js
+++ src/validation.js
@@ -2,9 +2,14 @@
  * 验证库
  * @namespace validation
  */
-
+import isCellphoneNumber from './validation/isCellphoneNumber';
 import isEmail from './validation/isEmail';
+import isIDCard from './validation/isIDCard';
+import isPostcode from './validation/isPostcode';
 
 export default {
+    isCellphoneNumber,
     isEmail,
+    isIDCard,
+    isPostcode,
 };
